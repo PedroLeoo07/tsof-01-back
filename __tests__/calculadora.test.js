2515caa88e7d51adeaf19884bbcd820519783d80
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-08-26 14:34:43 -0300

    feat: adicionar função de subtração e testes correspondentes à calculadora

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,13 +1,31 @@
-import { somar } from '../src/calculadora.js';
+import { somar, subtrair } from '../src/calculadora.js';
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
-
+        //AAA
+        //Arrange (Preparar)
         const a = 5;
         const b = 7;
 
+        //Act (Executar)
         const resultado = somar(a, b);
 
+        //Assert (Verificar)
         expect(resultado).toBe(12);
+    })
+})
+
+describe("Calculadora", () => {
+    test("Deve subtrair dois números corretamente", () => {
+        //AAA
+        //Arrange (Preparar)
+        const a = 5;
+        const b = 7;
+
+        //Act (Executar)
+        const resultado = subtrair(a, b);
+
+        //Assert (Verificar)
+        expect(resultado).toBe(-2);
     })
 })
--- src/calculadora.js
+++ src/calculadora.js
@@ -1,3 +1,9 @@
+// Função para somar dois números
 export function somar(a, b) {
     return a + b;   
+}
+
+// Função para subtrair dois números
+export function subtrair(a, b) {
+    return a - b;
 }
